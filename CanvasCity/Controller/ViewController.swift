2f5fd144ab434a9d7271f6688399d98323a2909b
devxris <chuwei0915@gmail.com>
2017-10-27 13:58:04 +0800

    Assets and MVC setup

---
+++ CanvasCity/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CanvasCity
+//
+//  Created by Chris Huang on 27/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
