2510e70e271b6f39722704b36d5b8291540c0fae
devxris <chuwei0915@gmail.com>
2017-10-27 15:49:07 +0800

    Centering user current location

--- CanvasCity/Controller/MapVC.swift
+++ CanvasCity/Controller/MapVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
@@ -18,14 +19,49 @@ class MapVC: UIViewController {
 		}
 	}
 	
+	// variables
+	let locationManager = CLLocationManager()
+	let authorizationStatus = CLLocationManager.authorizationStatus()
+	let regionRadius: Double = 1000
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		locationManager.delegate = self
+		configureLocationServices()
+	}
 
 	// target actions
 	@IBAction func centerMap(_ sender: UIButton) {
+		if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+			centerMapOnUserLocation()
+		}
 	}
-	
 }
 
 extension MapVC: MKMapViewDelegate {
 	
+	// customized function
+	func centerMapOnUserLocation() {
+		guard let coordinate = locationManager.location?.coordinate else { return } // get user current location
+		let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2, regionRadius * 2)
+		mapView.setRegion(coordinateRegion, animated: true)
+	}
+}
+
+extension MapVC: CLLocationManagerDelegate {
+	
+	// customized function
+	func configureLocationServices() {
+		if authorizationStatus == .notDetermined {
+			locationManager.requestAlwaysAuthorization()
+		} else {
+			return
+		}
+	}
+	
+	// CLLocationManagerDelegate functions
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		centerMapOnUserLocation()
+	}
 }
 
